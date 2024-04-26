4acce6fdf8872969abbbaae56e449dd708e44158
Anton Jeppsson <anton@ontanj.se>
2024-04-26 08:22:15 +0200

    Add example output

---
+++ examples/no-transform.out.js
@@ -0,0 +1,7 @@
+export function noRecursion(a) {
+  return func(a);
+}
+export function notTail(a) {
+  const c = notTail(a);
+  return c;
+}
---
+++ examples/transform.out.js
@@ -0,0 +1,42 @@
+export function base(a) {
+  while (true) {
+    if (a <= 0) {
+      return a;
+    }
+    a = a - 1;
+  }
+}
+export function multipleArgs(a, b) {
+  while (true) {
+    if (a < b) return a;
+    a = a - 1;
+    b = b + 1;
+  }
+}
+export function defaultArgInactive(a, b = 0) {
+  while (true) {
+    if (a < b) return a;
+    a = a - 1;
+    b = b + 1;
+  }
+}
+export function defaultArgActive(a, b = 0) {
+  while (true) {
+    if (a < b) return a;
+    a = a - 1;
+    b = 0;
+  }
+}
+export function undefinedArg(a, b) {
+  while (true) {
+    if (a < b) return a;
+    a = a - 1;
+    b = undefined;
+  }
+}
+export const arrowFunction = a => {
+  while (true) {
+    if (a <= 0) return a;
+    a = a - 1;
+  }
+};
