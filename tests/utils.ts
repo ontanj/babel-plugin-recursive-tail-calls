1418fd9fc08e279224df40f45ed8b5ba5c45e82c
Anton Jeppsson <anton@ontanj.se>
2024-05-14 12:27:27 +0200

    Convert test utils to TypeScript

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,4 +1,4 @@
-export function mapToTestCases(cases) {
+export function mapToTestCases(cases: Record<string, Function>) {
   return Object.entries(cases)
     .map(([name, func]) => [name, func.toString()])
     .map(([name, func]) =>
